f30a1c4ddc78dee3a0af8bcf10c001b70393e623
Robin Su <robisu@pdx.edu>
2020-07-27 15:44:49 -0700

    commented out API stuff

--- express-backend/app.js
+++ express-backend/app.js
@@ -1,45 +1,48 @@
 // import modules
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const cors = require("cors");
-const { urlencoded } = require("express");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cors = require('cors');
+const { urlencoded } = require('express');
 
-//const trefle = require("./api/trefle");
-//const weather = require("./api/weather");
+//const trefle = require('./api/trefle');
+//const weather = require('./api/weather');
 
 // import router paths
-let indexRouter = require("./routes/index");
-let searchRouter = require("./routes/search");
-let plantRouter = require("./routes/plants");
-let findRouter = require("./routes/plantfinder");
-let weatherRouter = require("./routes/weather");
-let aboutRouter = require("./routes/about");
+let indexRouter = require('./routes/index');
+let searchRouter = require('./routes/search');
+let plantRouter = require('./routes/plants');
+let findRouter = require('./routes/plantfinder');
+let weatherRouter = require('./routes/weather');
+let aboutRouter = require('./routes/about');
+
 
 // create express app object
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.use(cors());
 // call middleware functions for each requested path
-app.use("/", indexRouter);
-app.use("/search", searchRouter);
-app.use("/plants", plantRouter);
-app.use("/plantfinder", findRouter);
-app.use("/weather", weatherRouter);
-app.use("/about", aboutRouter);
+app.use('/', indexRouter);
+app.use('/search', searchRouter);
+app.use('/plants', plantRouter);
+app.use('/plantfinder', findRouter);
+app.use('/weather', weatherRouter);
+app.use('/about', aboutRouter);
+
 
 // middleware functions
 // parsing urlencoded payloads
 app.use(logger("dev"));
 app.use(express.json());
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({extended: false}));
 
 // if 404 error
 app.use(function (req, res, next) {
