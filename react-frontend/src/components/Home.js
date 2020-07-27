4fa694b66574613954eedc759d0505c18d8d7b65
Robin Su <robisu@pdx.edu>
2020-07-27 16:13:46 -0700

    component skeletons added

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import '../App.css';
+import '../stylesheets/Home.css';
 
 class Home extends React.Component {
   render() {
     return <div>
-        <form action="#">
+        <form action="#" >
         <div class="form-group m-5 p-5 text-center">
             <label for="search" class="lead mb-3 display-4">Enter plant to care for:</label>
             <input type="text" id="search" class="form-control m-2" placeholder="search any plant..." required />
