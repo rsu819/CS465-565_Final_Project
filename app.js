d95df5f8b424e90944d6a4a1a411777f4f49defc
Jean Choi <choijean@pdx.edu>
2020-07-27 22:34:15 -0700

    change filetree

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ let aboutRouter = require("./routes/about");
 // create express app object
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "react-frontend/build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/react-frontend/build/index.html"));
 });
 
 app.use(cors());
