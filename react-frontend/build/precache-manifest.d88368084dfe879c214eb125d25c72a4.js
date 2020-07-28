5fde6daa58ceb456a580d9002423cb81319d3150
Jean Choi <choijean@pdx.edu>
2020-07-27 23:04:07 -0700

    remove build from .gitignore

---
+++ react-frontend/build/precache-manifest.d88368084dfe879c214eb125d25c72a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3fdbd5b5bf577c278cd157d994bc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a934ebb3d3a9372d916",
+    "url": "/static/css/main.f64f67f3.chunk.css"
+  },
+  {
+    "revision": "4b6b9f9b6fc3c18bb5d7",
+    "url": "/static/js/2.7198b573.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7198b573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a934ebb3d3a9372d916",
+    "url": "/static/js/main.740dcb52.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "e7c0b30fd9c6cc03ff326e0e28bffeb8",
+    "url": "/static/media/jean_choi.e7c0b30f.png"
+  }
+]);
