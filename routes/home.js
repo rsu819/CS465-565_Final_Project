4ef2d3f4f9baa6dfc09c0d9c17cd5c96b49a7250
Robin Su <robisu@pdx.edu>
2020-07-29 20:14:02 -0700

    home typo

---
+++ routes/home.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+
+//var trefle = require('../api/trefle');
+
+var jwt = "";
+
+// router.use(function getAuth(req, res, next) {
+//     trefle.getAuth().then(response => {
+//         jwt = response.token;
+//         next()});
+// }, function (req, res) {
+//     trefle.requestAll(jwt).then(data => res.send(data));
+// });
+
+// router.use(function getAuth(req, res, next) {
+//     trefle.getAuth().then(response => {
+//         jwt = response.token;
+//         res.sendStatus(200)});
+// });
+
+module.exports = router;
