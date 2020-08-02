8b6ae510f4e0b851c02b34b16c7cc19e9c1a740c
Jean Choi <choijean@pdx.edu>
2020-08-01 18:30:50 -0700

    building files before deployment

---
+++ react-frontend/build/precache-manifest.5b177e1d7149a0a338e1668778f42c6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3928001327914312f7aeb7caf75478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82058c691fe62954f124",
+    "url": "/static/css/main.47154740.chunk.css"
+  },
+  {
+    "revision": "392c3ec990db39f8eb91",
+    "url": "/static/js/2.9ab5dede.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9ab5dede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82058c691fe62954f124",
+    "url": "/static/js/main.36f98ec6.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "e7c0b30fd9c6cc03ff326e0e28bffeb8",
+    "url": "/static/media/jean_choi.e7c0b30f.png"
+  }
+]);
