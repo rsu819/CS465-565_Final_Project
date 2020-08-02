6d6d7ac3715013fbcabb9f06ea89d1d473afd498
Jean Choi <choijean@pdx.edu>
2020-08-01 17:53:29 -0700

    last heroku test

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 
 if (process.env.NODE_ENV === 'production') {
 
-  app.use(express.static(path.join(__dirname, "react-frontend/", "build")));
+  app.use(express.static(path.join(__dirname, "react-frontend", "build")));
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
