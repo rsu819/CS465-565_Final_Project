7179db4d629754723c40ab430ba300a5cc717cbe
Jean Choi <choijean@pdx.edu>
2020-08-05 13:02:19 -0700

    initial redirect to /home instead of /search

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.redirect('./search');
+  res.redirect('./home');
 });
 
 module.exports = router;
