3dfbe35f6b4ac1f7cff0e5a5be516fe8b45b3397
Jean Choi <choijean@pdx.edu>
2020-08-07 18:18:58 -0700

    add disables to finder

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -14,6 +14,7 @@ import PageNotFound from "./components/PageNotFound";
 class App extends React.Component {
   componentDidMount() {
     let token = JSON.parse(localStorage.getItem('user'));
+
     if (token) {
       console.log('we have a token!');
       console.log(token.token);
