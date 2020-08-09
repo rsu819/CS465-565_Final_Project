eea35939984df27c98c8dbde63bab2afe0136ae7
Jean Choi <choijean@pdx.edu>
2020-08-08 21:45:54 -0700

    update heroku post build script, remove api on about pg

--- routes/about.js
+++ routes/about.js
@@ -2,15 +2,4 @@ var express = require("express");
 const fetch = require("node-fetch");
 var router = express.Router();
 
-router.get("/", async function (req, res) {
-  try {
-    const response = await fetch(`https://trefle.io/api/v1/plants?token=${process.env.TREFLE_KEY}`)
-    const json = await response.json();
-    //console.log(json);
-    res.status(200).send(json);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
 module.exports = router;
