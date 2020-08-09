ab35820b7202b94e5cd32dba378d90d0cfd49a11
Jean Choi <choijean@pdx.edu>
2020-08-08 22:19:14 -0700

    remove console.logs in plants.js

--- routes/plants.js
+++ routes/plants.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 let express = require('express');
 let router = express.Router();
-let fetch = require('node-fetch'); 
+let fetch = require('node-fetch');
 let parser = require('body-parser');
 const app = require('../app');
 
@@ -16,71 +16,70 @@ let plants = `/api/v1/plants/`;
 
 router.use(parser.json());
 
-router.post('/next', async function(req, res) {
+router.post('/next', async function (req, res) {
   try {
     let request = await req.body.endpoint;
-    console.log(request);
+    // console.log(request);
     let path = await request;
-  //console.log(req.body.path);
- 
-    const response = await fetch(url+path+`&token=${process.env.TREFLE_KEY}`);
+    //console.log(req.body.path);
+
+    const response = await fetch(url + path + `&token=${process.env.TREFLE_KEY}`);
     const json = await response.json();
-    console.log(json);
+    // console.log(json);
     res.status(200).send(json);
   }
-  catch(error) {
-    console.log(error);
+  catch (error) {
+    // console.log(error);
     res.status(400).send(error);
   }
 });
 
-router.get('/:slug', async function(req, res) {
+router.get('/:slug', async function (req, res) {
   let query = req.params.slug;
-  console.log('Search query: ' + query);
+  // console.log('Search query: ' + query);
   try {
-    const response = await fetch(url+search+query+`&token=${process.env.TREFLE_KEY}`);
+    const response = await fetch(url + search + query + `&token=${process.env.TREFLE_KEY}`);
     const json = await response.json();
-    console.log(json);
+    // console.log(json);
     res.status(200).send(json);
   }
-  catch(error) {
+  catch (error) {
     console.log(error);
   }
-  
+
 });
 
 // get information for genus list
-router.get('/family/:name', async function(req, res) {
+router.get('/family/:name', async function (req, res) {
   let query = req.params.name;
-  console.log('Search family: ' + query);
+  // console.log('Search family: ' + query);
   try {
-    const response = await fetch(url+plants+`?token=${process.env.TREFLE_KEY}&filter[family_common_name]=${query}`);
-  const json = await response.json();
-  console.log(json);
-  res.status(200).send(json);
+    const response = await fetch(url + plants + `?token=${process.env.TREFLE_KEY}&filter[family_common_name]=${query}`);
+    const json = await response.json();
+    // console.log(json);
+    res.status(200).send(json);
   }
-  catch(error) {
-    console.log(error);
+  catch (error) {
+    // console.log(error);
     res.send(err);
   }
 });
 
 // get information for selected plant
-router.get('/:slug/:id', async function(req, res) {
+router.get('/:slug/:id', async function (req, res) {
   let query = req.params.id;
-  console.log('Search id: ' + query);
+  // console.log('Search id: ' + query);
   try {
-    const response = await fetch(url+plants+query+`?token=${process.env.TREFLE_KEY}`);
-  const json = await response.json();
-  console.log(json);
-  res.status(200).send(json);
+    const response = await fetch(url + plants + query + `?token=${process.env.TREFLE_KEY}`);
+    const json = await response.json();
+    // console.log(json);
+    res.status(200).send(json);
   }
-  catch(error) {
+  catch (error) {
     console.log(error);
   }
 });
 
 
 
-  module.exports = router;
-  
+module.exports = router;
